6c66987dd772d609872b024c7b3297cf743aa894
FatalCodes <43865531+FatalCodes@users.noreply.github.com>
2024-02-23 19:05:15 -0500

    update

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
     $('#analyze-bet-btn-submit').click(function() {
       playerTeamName = document.getElementById('playerTeamNameEntry').value; // Direct assignment
-      console.log(playerTeamName); // Verify the assignment
       $('#playerTeamNameDisplay').text(playerTeamName); // Directly set the text
     });
-  });
+
+    var validator = $('#analyzeForm').validate();
+    $('#playerTeamNameEntry').rules({
+        name: {
+            required: true,
+            minlength: 2,
+            lettersonly: true
+        },
+
+        messages: {
+            name: {
+              required: 'Please enter your name',
+              minlength: 'Your name must be at least 2 characters long',
+              lettersonly: 'Your name must contain only letters'
+            }
+          }
+    });
+});
